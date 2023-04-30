c35fed57d75973cd5c41fca8b3fcbd1a444e0d03
cesarderio <raphaelchookagian@gmail.com>
2023-04-30 23:38:32 +0000

    adding more functions and playing around with the code flow.

--- Class05/Class05.sh
+++ Class05/Class05.sh
@@ -8,34 +8,75 @@
 # Write a script that displays running processes, asks the user for a PID, then kills the process with that PID. See README file for more in depth information.
 
 # Declaration of variables
+count=0
 
 # Declaration of functions
 
+doIt(){
+  read $userInput
+  if [  $userInput  ]
+    then
+      kill $( pgrep "$userInput" | grep -v grep | awk "{print $2}")
+  else
+    echo "Please enter a valid PID number"
+    killProcs
+  fi
+}
+
+killProcs(){
+  ps aux
+  echo "Choose target to eliminate"
+  echo "Enter PID"
+  read -r userInput
+  doIt
+  # kill $( pgrep "$userInput" | grep -v grep | awk "{print $2}")
+  count=$(( $count + 1 ))
+}
+
+runIt(){
+  count=0
+while (( $count < 3 ))
+  do
+    killProcs
+  done
+}
+
+killAgainQ(){
+  echo "Would you like to kill again? Yes/No"
+read -r userInput
+if [ "$userInput" = "yes" ]
+  then
+    echo "Maybe it's time for a break"
+elif [ "$userInput" = "no" ]
+  then
+    echo "Have a great day!"
+    exit
+else
+  runIt
+  killAgainQ
+fi
+}
 
 # Create
 
-
 # Main
 
-i=1
-
-while [ $i -le 3 ]
-  do
-    ps aux
-    echo "Choose target to eliminate"
-    echo "Enter PID"
-    read -r userInput
-    # kill "$userInput"
-    kill $( pgrep "$userInput" | grep -v grep | awk "{print $2}")
-    # ((i=i+1))
-    i=$(( $i + 1 ))
-  done
-   
-echo "Would you like to kill again? Yes/No"
-read -r userInput
-if [ $userInput="yes" ]
-  then
-    echo "time for a break"
-fi
+runIt
+killAgainQ
 
 # End
+
+#--------------------------------v1 below
+# i=1
+
+# while [ $i -le 3 ]
+#   do
+#     ps aux
+#     echo "Choose target to eliminate"
+#     echo "Enter PID"
+#     read -r userInput
+#     # kill "$userInput"
+#     kill $( pgrep "$userInput" | grep -v grep | awk "{print $2}")
+#     # ((i=i+1))
+#     i=$(( $i + 1 ))
+#   done
