0d77114776c6d594dd06929ef5aab8530204d7de
cesarderio <raphaelchookagian@gmail.com>
2023-05-03 02:05:05 +0000

    finishing up code challenge 07

--- Class07/Class07.sh
+++ Class07/Class07.sh
@@ -10,54 +10,37 @@
 
 
 # Declaration of functions
+Name() {
+echo "Name:"
+# sudo lshw -class Name ## <--------------------???????
+}
 
+CPU() {
+echo "CPU:"
+sudo lshw -class cpu | grep -wv capabilities | grep -wv version | grep -wv *-cpu
+}
+
+RAM() {
+echo "RAM:"
+sudo lshw -class memory
+}
+
+Display() {
+echo "Display Adapter:"
+sudo lshw -class display | grep -wv display | grep -wv version | grep -wv logical
+}
+
+Network() {
+echo "Network Adapter:"
+sudo lshw -class network | grep -wv network
+}
 # Create
 
 # Main
-#Name
-# sudo lshw -class Name
-
-# CPU
-echo "CPU:"
-sudo lshw -class cpu | grep -wv capabilities | grep -wv version
-
-echo "RAM:"
-sudo lshw -class cpu | grep -i description
-sudo lshw -class cpu | grep -i physical id
-sudo lshw -class cpu | grep -i size
-
-echo "Display Adapter:"
-sudo lshw -class display | grep -i description
-sudo lshw -class display | grep -i product
-sudo lshw -class display | grep -i vendor
-sudo lshw -class display | grep -i physical id
-sudo lshw -class display | grep -i bus info
-sudo lshw -class display | grep -i width
-sudo lshw -class display | grep -i clock
-sudo lshw -class display | grep -i capabilities
-sudo lshw -class display | grep -i configuration
-sudo lshw -class display | grep -i resources
-
-
-echo "Network Adapter:"
-sudo lshw -class network | grep -i description
-sudo lshw -class network | grep -i product
-sudo lshw -class network | grep -i vendor
-sudo lshw -class network | grep -i physical id
-sudo lshw -class network | grep -i bus info
-sudo lshw -class network | grep -i logical name
-sudo lshw -class network | grep -i version
-sudo lshw -class network | grep -i serial
-sudo lshw -class network | grep -i size
-sudo lshw -class network | grep -i capacity
-sudo lshw -class network | grep -i width
-sudo lshw -class network | grep -i clock
-sudo lshw -class network | grep -i capabilities
-sudo lshw -class network | grep -i configuration
-sudo lshw -class network | grep -i resources
-
-# sudo lshw -class CPU
-
-
+Name
+CPU
+RAM
+Display
+Network
 
 # End
