e1fc212a9bd69c4f93f96de86e30bf563fb1b37e
Raphael Chookagian <raphaelchookagian@gmail.com>
2023-05-09 12:51:50 -0700

    setting up for code challenge 12

---
+++ Class12/Class12.ps1
@@ -0,0 +1,16 @@
+# Script Name:                  Powershell IP Analysis
+# Author:                       Raphael Chookagian
+# Date of latest revision:      05/9/2023
+# Purpose:                      Write a Powershell script that returns the IPv4 address of the computer.
+
+# Declare Functions
+# Create
+# Main
+
+# Create a Powershell script that performs the following operations:
+
+# Create a local file called network_report.txt that holds the contents of an ipconfig /all command.
+
+# Use Select-String to search network_report.txt and return only the IP version 4 address.
+
+# Remove the network_report.txt when you are finished searching it.
