a2cb89358ce71188333bab8da196bb577a74d76d
Raphael Chookagian <raphaelchookagian@gmail.com>
2023-05-09 13:17:01 -0700

    setting up for code challenge 12

--- Class12/Class12.ps1
+++ Class12/Class12.ps1
@@ -9,8 +9,19 @@
 
 # Create a Powershell script that performs the following operations:
 
+
+
+
 # Create a local file called network_report.txt that holds the contents of an ipconfig /all command.
+function NetReport {
+  ipconfig /all | Out-File -FilePath .\Desktop\network_report.txt
+}
+
+
 
 # Use Select-String to search network_report.txt and return only the IP version 4 address.
+Select-String -Path .\Desktop\network_report.txt -Pattern 'IPV4'
+
 
 # Remove the network_report.txt when you are finished searching it.
+Remove-Item -Path .\Desktop\network_report.txt -Force
