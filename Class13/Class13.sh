1da7db95e352df333ee940bf9f6e0129eabd4280
Raphael Chookagian <raphaelchookagian@gmail.com>
2023-05-10 13:11:36 -0700

    setting up Ops code challenge 13

--- Class13/Class13.sh
+++ Class13/Class13.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 # Script Name:                  Domain Analyzer
 # Author:                       Raphael Chookagian
 # Date of latest revision:      05/10/2023
@@ -22,11 +24,55 @@
 
 
 # Declare Variables
+# AddToFile=">> fetchDom.txt"
+
+
 
 # Declare Functions
+removeDom(){
+  rm -rf fetchDom.txt
+}
+
+WhoDis(){
+  whois "$userInput"
+  whois "$userInput" >> fetchDom.txt
+}
+
+DigDis(){
+  dig "$userInput"
+  dig "$userInput" >> fetchDom.txt
+}
+
+HostDis(){
+  host "$userInput"
+  host "$userInput" >> fetchDom.txt
+}
+
+NslDis(){
+  nslookup "$userInput"
+  nslookup "$userInput" >> fetchDom.txt
+}
+
 
 # Create
-
+removeDom
 # Main
 
 # End
+fetchDom(){
+  echo "Please enter a domain name"
+  read -r userInput 
+  WhoDis
+  # whois "$userInput"
+  # whois "$userInput" >> fetchDom.txt
+  DigDis
+  # dig "$userInput"
+  # dig "$userInput" >> fetchDom.txt
+  HostDis
+  # host "$userInput"
+  # host "$userInput" >> fetchDom.txt
+  NslDis
+  # nslookup "$userInput"
+  # nslookup "$userInput" >> fetchDom.txt
+}
+fetchDom
