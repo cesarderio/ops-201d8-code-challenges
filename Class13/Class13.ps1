a7703dee2bd64b72b1fb5f444cfabbfd0d001e61
Raphael Chookagian <raphaelchookagian@gmail.com>
2023-05-10 12:08:14 -0700

    setting up Ops code challenge 13

---
+++ Class13/Class13.ps1
@@ -0,0 +1,32 @@
+# Script Name:                  Domain Analyzer
+# Author:                       Raphael Chookagian
+# Date of latest revision:      05/10/2023
+# Purpose:                      Create a script that asks a user to type a domain, then displays information about the typed domain. Operations that must be used include:
+  # whois
+  # dig
+  # host
+  # nslookup
+
+  # Install whois on your Ubuntu VM.
+
+  # Add to your bash script a sixth option that calls a function to:
+  
+  # Take a user input string. Presumably the string is a domain name such as Google.com.
+  # Run whois against a user input string.
+  # Run dig against the user input string.
+  # Run host against the user input string.
+  # Run nslookup against the user input string.
+  # Output the results to a single .txt file and open it with your favorite text editor.
+
+
+
+
+# Declare Variables
+
+# Declare Functions
+
+# Create
+
+# Main
+
+# End
